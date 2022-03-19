8dc452aca270953d57f035627affd184a968c539
oren <oren@sogo.co.il>
2022-03-19 09:52:07 +0200

    change route

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,23 +1,23 @@
-const jwt = require('jsonwebtoken')
-const {User} = require('../modules/users')
+const jwt = require("jsonwebtoken");
+const User = require("../modules/users");
 
-const auth  =async (req, res, next)=>{
-   try {
-       const token = req.header('Authorization').replace('Bearer ', '')
-       const decode = await jwt.verify(token,'gthisismysecret')
-      const user = await User.findOne({_id: decode._id, 'tokens.token': token})
-      
-      if(!user){
-          throw new Error()
-      }
-      req.user = user
-      req.token = token
-      next()
-   } catch (error) {
-       res.status(401).send(error.message  + "Please authenticate")
-   }
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decode = await jwt.verify(token, "gthisismysecret");
   
-   
-}
+    const user = await User.findOne({ _id: decode._id, "tokens.token": token });
 
-module.exports = auth
+    if (!user) {
+      throw new Error();
+    }
+    req.user = user;
+    req.token = token;
+    console.log(req.user)
+    next();
+  } catch (error) {
+    res.status(401).send(error.message + "Please authenticate");
+  }
+};
+
+module.exports = auth;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,26 +1,97 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
 
-const {
-    allUsers,
-    me,
-    createUser,
-    getUser,
-    updateUser,
-    deleteUser,
-    login,
-    logout,
-    logoutAll
-} = require('../modules/users')
+const User = require("../modules/users");
 
-router.route('/login').post(login)
-router.route('/logout').post(auth, logout)
-router.route('/logoutAll').post(auth, logoutAll)
-router.route('/me').get(auth, me)
-router.route('/').get(auth, allUsers).post(createUser)
-router.route('/:id').get(auth, getUser).patch(auth, updateUser).delete(auth, deleteUser)
+router.route("/login").post(async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.username,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
 
-module.exports = router
+router.route("logout").post(auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => {
+      return token.token !== req.token;
+    });
 
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
 
+router.route("/logoutAll").post(auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+
+    await req.user.save();
+    res.send();
+  } catch (error) {
+    res.status(401).send(error.message);
+  }
+});
+
+router.route("/").get(auth, async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(400).send();
+  }
+});
+
+router.route("/me").get(auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.route("/me").patch(auth, async (req, res) => {
+  try {
+    const user = req.user;
+    if (!user) {
+      res.status(404).send();
+    }
+    const params = Object.keys(req.body);
+    params.forEach((param) => {
+      user[param] = req.body[param];
+    });
+    // console.log(user)
+    await user.save();
+
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+router.route("/").delete(auth, async (req, res) => {
+  try {
+    const user = req.user;
+    user.deleteOne();
+    res.status(200).send();
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+module.exports = router;
